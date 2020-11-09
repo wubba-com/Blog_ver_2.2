5008317b6bf6839cd501d54ab7a2c74a1301fdd5
Sergey <eng.ise8@gmail.com>
2020-11-09 17:36:46 +0200

    create classes for models, added search and interface

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Post, Tags
+from django.utils.safestring import mark_safe
 
 
-admin.site.register(Post)
-admin.site.register(Tags)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'get_image')
+    save_on_top = True
+    list_filter = ('title',)
+    search_fields = ('title', 'tags')
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width=50px height=50px')
+
+    get_image.short_description = 'Изображение'
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    save_on_top = True
+    list_filter = ('url_tags',)
+    search_fields = ('title',)
+
+
+admin.site.site_title = 'Обработка контента'
+admin.site.site_header = 'Обработка контента'
